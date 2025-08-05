eaf05ae384c4259882c86642c4e0f4186ee63490
chalormpol <chalormpol9899@gmail.com>
2025-08-06 00:59:07 +0700

    build

--- app/apiConfig.ts
+++ app/apiConfig.ts
@@ -1,7 +1,7 @@
 import Swal from "sweetalert2";
 
 export const apiConfig = {
-  apiUrl: "https://workshopbunnext2024api-production.up.railway.app/",
+  apiUrl: "https://workshopbunnext2024api-production.up.railway.app",
   tokenKey: "token_bun_service",
   confirmDialog: () => {
     return Swal.fire({
